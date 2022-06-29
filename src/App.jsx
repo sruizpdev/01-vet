243eee6662b53a3f14ea061c7e0ff2e113eaab80
sruizpdev <sergiotto@outlook.com>
2022-06-29 19:06:50 +0200

    first skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,15 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import Form from "./components/Form";
+import Header from "./components/Header";
+import Patients from "./components/Patients";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div className="container mx-auto mt-10">
+      <Header />
+      <Form />
+      <Patients />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <h1 className="text-center font-black text-5xl md:w-2/3 mx-auto">
+      Seguimiento de pacientes{" "}
+      <span className="text-indigo-600">Veterinaria</span>
+    </h1>
+  );
+}
+
+export default Header;
